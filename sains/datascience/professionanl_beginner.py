aadc7703b38f13ad4af18f1600f0a1415779f93e
syukurd <syukurbjj@yahoo.com>
2021-06-17 19:38:08 +0700

    studi kasus

--- sains/datascience/professionanl_beginner.py
+++ sains/datascience/professionanl_beginner.py
@@ -3,3 +3,15 @@
 person = {'nama' : 'John Doe','pekerjaan':'Programmer'}
 print(person['nama'])
 print(person['pekerjaan'])
+
+bilangan = (5 % 3 ** 2) + (3 + 2 * 2) * (4 - 2) 
+print(bilangan)
+
+
+tagihan_ke = 'Mr. Yoyo'
+warehousing = { 'harga_harian': 1000000, 'total_hari':15 } 
+cleansing = { 'harga_harian': 1500000, 'total_hari':10 } 
+integration = { 'harga_harian':2000000, 'total_hari':15 } 
+transform = { 'harga_harian':2500000, 'total_hari':10 }
+sub_warehousing = warehousing['harga_harian']
+print(sub_warehousing)
