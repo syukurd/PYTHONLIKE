cc4614cf6557bc48d633db0cc4fce1a87b046c56
syukurd <syukurbjj@yahoo.com>
2021-06-17 09:14:46 +0700

    data wraling

--- sains/datascience/panda.py
+++ sains/datascience/panda.py
@@ -7,4 +7,13 @@ table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gende
 table.head()
 x_label = table['NAMA KELURAHAN']
 plt.bar(x=np.arange(len(x_label)),height=table['LAKI-LAKI WNI'])
+plt.xticks(np.arange(len(x_label)),x_label,rotation = 20)
+plt.xlabel('Keluarahan di Jakarta pusat')
+plt.ylabel('Jumlah Penduduk Laki - Laki')
+plt.title('Persebaran Jumlah Penduduk Laki- Laki di Jakarta Pusat')
 plt.show()
+
+
+print(table.columns) #menampilkan column
+print(table['Age']) #menampilkan isi dari age
+print(table.iloc[5]) #menampilkan isi dari baris
