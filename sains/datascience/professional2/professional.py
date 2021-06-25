ec2e210cfd89ff27ca41a08b9105686cf8595b62
syukurd <syukurbjj@yahoo.com>
2021-06-25 13:20:28 +0700

    index,insert,pop,remove,sort,

--- sains/datascience/professional2/professional.py
+++ sains/datascience/professional2/professional.py
@@ -30,3 +30,8 @@ list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
 list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
 list_menu.extend(list_minuman)
 print(list_menu)
+
+
+
+
+# index,insert,pop,remove,sort,
