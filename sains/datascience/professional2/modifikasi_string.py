89f7fa375d6a159eceb06e45d39f57639c93bb9d
syukurd <syukurbjj@yahoo.com>
2021-06-28 09:07:15 +0700

    manipulasi string

---
+++ sains/datascience/professional2/modifikasi_string.py
@@ -0,0 +1,30 @@
+# Fitur .split()
+print(">>> Fitur .split()")
+bilangan = "ani dan budi dan wati dan johan"
+karakter = bilangan.split("dan")
+print(karakter)
+kata = bilangan.split(" ")
+print(kata)
+# Fitur .join()
+print(">>> Fitur .join()")
+pemisah = " dan "
+karakter = ["Ricky", "Peter", "Jordan"]
+kalimat = pemisah.join(karakter)
+print(kalimat)
+
+
+# Fitur .replace()
+print(">>> Fitur .replace()")
+kalimat = "apel malang apel yang paling segar, apel sehat, apel nikmat"
+kalimat = kalimat.replace("apel","jeruk")
+print(kalimat)
+
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+# Fitur .find()
+print(">>> Fitur .find()")
+print(teks.find("Apel"))
+print(teks.find("malang"))
+# Fitur .count()
+print(">>> Fitur .count()")
+kemunculan_kata_apel = teks.count("apel")
+print(kemunculan_kata_apel)
--- sains/datascience/professional2/quis.py
+++ sains/datascience/professional2/quis.py
@@ -1,2 +1,4 @@
-barisan = [1,2,3,4,5]
-print(barisan[-2])
+nama_produk = "Sepatu Niko"
+nama_produk = nama_produk[7:] + "P" + nama_produk [3:9] + "K" + nama_produk[-1]
+
+print(nama_produk[:7])
