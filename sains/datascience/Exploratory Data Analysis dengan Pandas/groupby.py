9342a69bf858153d1689985b30cfa1db9715d7f2
syukurd <syukurbjj@yahoo.com>
2021-07-14 23:32:06 +0700

    menyelesaikan tugas praktek

--- sains/datascience/Exploratory Data Analysis dengan Pandas/groupby.py
+++ sains/datascience/Exploratory Data Analysis dengan Pandas/groupby.py
@@ -6,3 +6,8 @@ import pandas as pd
 order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
 rata_rata = order_df["price"].groupby(order_df["payment_type"]).mean()
 print(rata_rata)
+
+# “Aksara, ini sudah menyeluruh dan lengkap. Tambahin satu lagi saja, tolong cari berapa harga maksimum pembelian customer di dataset order_df.”
+# “Oke.”
+sort_harga = order_df.sort_values("price",ascending=False)
+print(sort_harga)
