e951b7ff84970d51c39f10f7ef3339d0e3781ee7
syukurd <syukurbjj@yahoo.com>
2021-07-14 22:52:28 +0700

    melakukan rename pada kolom

--- sains/datascience/Exploratory Data Analysis dengan Pandas/quantile.py
+++ sains/datascience/Exploratory Data Analysis dengan Pandas/quantile.py
@@ -6,3 +6,8 @@ Q3 = order_df[["product_weight_gram"]].quantile(0.75)
 
 IQR = Q3-Q1
 print(IQR)
+
+
+# Ganti nama kolom freight_value menjadi shipping_cost
+order_df.rename(columns={"freight_value": "shipping_cost"}, inplace=True)
+print(order_df)
