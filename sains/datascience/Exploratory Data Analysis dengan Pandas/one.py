cbcaa1ac5673b3379fb4bec24669debf13995b0c
syukurd <syukurbjj@yahoo.com>
2021-07-14 22:19:47 +0700

    menambah gambar deviasi

--- sains/datascience/Exploratory Data Analysis dengan Pandas/one.py
+++ sains/datascience/Exploratory Data Analysis dengan Pandas/one.py
@@ -6,6 +6,7 @@
 # tampilkan 10 data teratas menggunakan head()
 from numpy import median
 import pandas as pd
+import matplotlib.pyplot as plt
 order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
 print(order_df.head())
 
@@ -15,3 +16,8 @@ print(order_df.describe())
 
 #menampilkan rata rata median dari tabel colom price
 print(order_df.loc[:, "price"].median())
+
+
+#saya mau menampilkan histogram dari price
+order_df[["price"]].hist(figsize=(4,5),bins=10,xlabelsize=8,ylabelsize=8)
+plt.show()
