8831cf55deb398f6169d465f460c017809e6d97f
syukurd <syukurbjj@yahoo.com>
2021-07-14 16:52:01 +0700

    melakukan summary dan menampilkan data rata-rata median

--- sains/datascience/Exploratory Data Analysis dengan Pandas/one.py
+++ sains/datascience/Exploratory Data Analysis dengan Pandas/one.py
@@ -1,9 +1,17 @@
-import pandas as pd
-order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
-print(order_df.shape)
+# import pandas as pd
+# order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
+# print(order_df.shape)
 
 
 # tampilkan 10 data teratas menggunakan head()
+from numpy import median
 import pandas as pd
 order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
-print(order_df.head(10))
+print(order_df.head())
+
+
+# menampilkan summary dari statistic data
+print(order_df.describe())
+
+#menampilkan rata rata median dari tabel colom price
+print(order_df.loc[:, "price"].median())
