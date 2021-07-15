da42add90f6569ffada33b1f7ab6f3974e7010e2
syukurd <syukurbjj@yahoo.com>
2021-07-15 23:42:38 +0700

    alternatif membuat sumbu

--- sains/datascience/data visualisasi/dataset.py
+++ sains/datascience/data visualisasi/dataset.py
@@ -23,3 +23,8 @@ print(monthly_amount)
 import matplotlib.pyplot as plt
 plt.plot(monthly_amount['order_month'], monthly_amount['gmv'])
 plt.show()
+
+# cara simple menampilkan grafik
+
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.show()
