437adbd6be1706db5ecd9c3d9a345847179cec8f
syukurd <syukurbjj@yahoo.com>
2021-07-15 14:47:05 +0700

    datetime note

--- sains/datascience/data visualisasi/dataset.py
+++ sains/datascience/data visualisasi/dataset.py
@@ -1,4 +1,15 @@
 import pandas as pd
+
+import datetime
 dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
 print('%d baris dan %d kolom \n' % dataset.shape)
 print(dataset.head())
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+print(dataset.head())
+
+
+# Ada beberapa function yang berperan di sini:
+
+# apply & lambda biasa digunakan untuk membuat kolom baru, berdasarkan suatu kolom lain yang sudah ada (misal .apply(lambda x: x*2) berarti setiap input x di dalam kolom, akan diubah menjadi x*2). Dalam hal ini kolom yang sudah ada adalah dataset['order_date'], lalu tiap nilai di dalamnya kita proses agar menjadi month-nya saja
+# Function datetime.datetime.strptime digunakan untuk mengubah date/time dalam bentuk string menjadi tipe data datetime.
+# Function  strftime digunakan untuk mengubah format suatu data bertime datetime, dalam hal ini diubah menjadi '%Y-%m', yang berarti outputnya adalah waktu dengan bentuk YYYY-MM atau tahun dan bulan saja, tanggalnya sudah tidak ada. 
