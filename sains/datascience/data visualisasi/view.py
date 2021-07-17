74d0b8df1165b924ab34a032b3ae8fdd7c59c0f2
syukurd <syukurbjj@yahoo.com>
2021-07-17 17:00:11 +0700

    melakukan style pada grid

--- sains/datascience/data visualisasi/view.py
+++ sains/datascience/data visualisasi/view.py
@@ -6,9 +6,9 @@ dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime
 dataset['CMV'] = dataset['item_price'] * dataset['quantity']
 print(dataset.head())
 plt.figure(figsize=(15,5))
-a=dataset.groupby(['order_month'])['CMV'].sum().plot(linestyle='-.',marker='o')
+a=dataset.groupby(['order_month'])['CMV'].sum().plot(linestyle='-.',marker='o',color='green',linewidth =2)
 plt.title('Hello Dunia')
 plt.xlabel('Order_month', fontsize=12)
 plt.ylabel('Order_month', fontsize = 12) 
-
+plt.grid(color = 'darkgray',linewidth = 0.5,linestyle = ':' )
 plt.show()
